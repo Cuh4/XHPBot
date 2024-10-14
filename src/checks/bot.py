0514774c48fa1e9f85cca467c41af55f30dd1089
Cuh4 <89425262+Cuh4@users.noreply.github.com>
2024-10-14 23:15:48 +0100

    feat(checks): add app command (slash command) checks

---
+++ src/checks/bot.py
@@ -0,0 +1,38 @@
+# // ---------------------------------------------------------------------
+# // ------- [Checks] Bot
+# // ---------------------------------------------------------------------
+
+"""
+A bunch of slash command checks relating to the bot itself.
+Repo: https://github.com/Cuh4/XHPBot
+
+---
+
+Copyright (C) 2024 Cuh4
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+# ---- // Imports
+import discord
+from discord import app_commands
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from bot import Bot
+
+# ---- // Main
+def Ready(interaction: discord.Interaction) -> bool:
+    bot: Bot = interaction.client
+    return bot.Ready
