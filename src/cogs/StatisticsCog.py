4f8ee025c37afecd1bdda0faa58e0eb98338b6d8
Cuh4 <89425262+Cuh4@users.noreply.github.com>
2024-10-19 00:36:08 +0100

    feat(cogs): add `StatisticsCog` for tracking server statistics over time. this cog relies on `StatusCog` for fetching server info

---
+++ src/cogs/StatisticsCog.py
@@ -0,0 +1,90 @@
+# // ---------------------------------------------------------------------
+# // ------- [Cogs] Statistics Cog
+# // ---------------------------------------------------------------------
+
+"""
+A cog for tracking server statistics over time.
+Repo: https://github.com/Cuh4/XHPBot
+
+---
+
+Copyright (C) 2024 Cuh4
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+# ---- // Imports
+from discord.ext.tasks import loop
+import os
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from bot import Bot
+    from cogs.StatusCog import StatusCog
+
+from cogs.BaseCog import BaseCog
+
+from libs import print
+
+import models
+
+# ---- // Main
+class StatisticsCog(BaseCog):
+    """
+    A cog for tracking server statistics over time.
+    """
+    
+    def __init__(self, bot: "Bot"):
+        """
+        Initializes `StatisticsCog` class objects.
+        """        
+        
+        super().__init__(bot)
+
+        self.StatisticsLoop = loop(minutes = float(os.getenv("statistics_update_interval")))(self.UpdateStatistics)
+
+    # ---- // Callbacks
+    async def CogStartAsync(self):
+        self.StatusCog: "StatusCog" = self.Bot.get_cog("StatusCog")
+        self.StatisticsLoop.start()
+        
+    # ---- // Methods
+    async def UpdateStatistics(self):
+        """
+        Updates server statistics.
+        """        
+        
+        # Get server information
+        try:
+            server = await self.StatusCog.FetchServerInformation()
+        except Exception as error:
+            print.error(self.qualified_name, f"Failed to fetch server information: {error}")
+            server = None
+        
+        # Update statistics
+        try:
+            models.ServerStatistic.create_from_server(server)
+        except Exception as error:
+            print.error(self.qualified_name, f"Failed to update server statistics: {error}")
+            
+async def setup(bot: "Bot"):
+    """
+    Sets up the cog.
+    Called automatically by `bot.load_extension(...)`.
+
+    Args:
+        bot (Bot): The bot to provide to the cog.
+    """    
+    
+    await bot.add_cog(StatisticsCog(bot))
